85a565f7e626ee8cfe77c557ea75b58ffc0c5cbf
Horeb136 <horebseidou136@gmail.com>
2024-01-30 23:48:05 +0100

    Modification du README

--- crawler/README.md
+++ crawler/README.md
@@ -3,15 +3,31 @@
 ## Description
 Ce projet est un simple crawler web écrit en Python, conçu pour explorer et extraire des liens à partir d'un site web donné. Il prend en charge le crawling séquentiel ainsi que le crawling multi-thread pour une exécution plus rapide.
 
-## Contributeur
-- [Horeb SEIDOU](https://github.com/Horeb136)
-
 ## Prérequis
 - Python 3.x
 
 ## Installation
 1. Clonez ce dépôt : `git clone https://github.com/Horeb136/indexation-web.git`
 2. Accédez au répertoire du projet : `cd crawler`
+
+## Fonctionnalités
+1. **Exploration de liens** : Le crawler commence avec une URL de départ et explore les liens pour extraire de nouvelles URLs.
+
+2. **Respect du fichier `robots.txt`** : Avant de crawler une page, le crawler vérifie les règles spécifiées dans le fichier `robots.txt` du site pour respecter les restrictions.
+
+3. **Sitemap** : Le crawler prend en charge la découverte et le crawling des URLs à partir du fichier `sitemap.xml` d'un site.
+
+4. **Stockage des URLs visitées** : Les URLs visitées sont stockées dans un fichier texte (`crawled_webpages.txt`), permettant de garder une trace des pages explorées.
+
+5. **Base de données SQLite** : Les URLs et leur âge (temps écoulé depuis la visite) sont stockés dans une base de données SQLite (`crawler_db.sqlite`).
+
+6. **Crawling Multi-thread** : Le crawler peut être exécuté en mode multi-thread pour accélérer le processus de crawling.
+
+Structure du Projet
+1. `crawler.py` : Contient la classe `Crawler` avec les fonctionnalités de crawling.
+
+2. `main.py` : Fichier principal pour exécuter le crawler avec des arguments en ligne de commande.
+
 
 ## Utilisation
 Pour exécuter le crawler, ouvrez un terminal et exécutez le fichier `main.py` avec les arguments nécessaires.
@@ -26,4 +42,7 @@ python main.py https://example.com --max_urls 50 --politeness_delay 5
 python main.py https://example.com --max_urls 50 --politeness_delay 5 --multi_thread
 ```
 
+## Contributeur
+- [Horeb SEIDOU](https://github.com/Horeb136)
 
+Merci de contribuer à ce projet ! 🚀
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -13,11 +13,17 @@ from queue import Queue
 ssl._create_default_https_context = ssl._create_unverified_context
 
 class Crawler : 
-    def __init__(self, start_url, max_urls = 50, 
-                 politeness_delay = 5, 
-                 db_name = "crawler/crawler_db.sqlite", 
-                 num_threads = 5, 
-                 ) : 
+    def __init__(self, start_url, max_urls=50, politeness_delay=5, db_name="crawler/crawler_db.sqlite", num_threads=5) : 
+        """
+        Initialiseur de la classe Crawler.
+
+        Paramètres :
+        - start_url : URL de départ pour le crawling.
+        - max_urls : Nombre maximum d'URLs à crawler.
+        - politeness_delay : Délai de politesse en secondes entre les requêtes.
+        - db_name : Nom de la base de données SQLite pour stocker les données.
+        - num_threads : Nombre de threads pour le crawling multi-threaded.
+        """
         self.start_url = start_url 
         self.max_urls = max_urls 
         self.politeness_delay = politeness_delay 
@@ -25,7 +31,7 @@ class Crawler :
         self.to_visit_urls = [start_url]
         self.crawled_urls = []
         self.last_page_download_time = 0
-        #Initialiser la connexion à la base de données
+        # Initialiser la connexion à la base de données
         self.conn = sqlite3.connect(db_name)
         self.create_table()
         self.timestamp = int(time.time())
@@ -33,13 +39,30 @@ class Crawler :
         self.num_threads = num_threads
         self.thread_queue = Queue()
         self.lock = threading.Lock()
-        #self.connections = [sqlite3.connect(db_name) for _ in range(num_threads)]
 
     def is_valid_url(self, url) : 
+        """
+        Vérifie si une URL est valide.
+
+        Paramètres :
+        - url : URL à vérifier.
+
+        Retourne :
+        True si l'URL est valide, False sinon.
+        """
         parsed_url = urlparse(url)
         return bool(parsed_url.netloc) and bool(parsed_url.scheme)
     
     def can_crawl(self, base_url) :
+        """
+        Vérifie si la page à l'URL donnée peut être crawlée en fonction du fichier robots.txt.
+
+        Paramètres :
+        - base_url : URL de la page à vérifier.
+
+        Retourne :
+        True si la page peut être crawlée, False sinon.
+        """
         if not self.is_valid_url(base_url):
             return False
         else :  
@@ -51,12 +74,21 @@ class Crawler :
             return rp.can_fetch("*", base_url)
     
     def fetch_page(self, url) : 
+        """
+        Récupère le contenu HTML d'une page à partir de son URL.
+
+        Paramètres :
+        - url : URL de la page à récupérer.
+
+        Retourne :
+        Le contenu HTML de la page si la requête est réussie, None sinon.
+        """
         try : 
             start_time = time.time()
             response = requests.get(url)
             end_time = time.time()
             if response.status_code == 200 : 
-                self.last_page_dowload_time = end_time - start_time
+                self.last_page_download_time = end_time - start_time
                 return response.text
             else : 
                 return None
@@ -65,7 +97,15 @@ class Crawler :
             return None
     
     def fetch_sitemap(self, sitemap_url) : 
-        #sitemap_url = urljoin(base_url, "/sitemap.xml")
+        """
+        Récupère la liste des URLs à partir d'un fichier sitemap XML.
+
+        Paramètres :
+        - sitemap_url : URL du fichier sitemap XML.
+
+        Retourne :
+        Liste des URLs du sitemap.
+        """
         try :
             response = requests.get(sitemap_url)
             if response.status_code == 200 :
@@ -81,18 +121,27 @@ class Crawler :
     
 
     def extract_links(self, html_contents):
+        """
+        Extrait les liens (URLs) du contenu HTML de la page.
+
+        Paramètres :
+        - html_contents : Contenu HTML de la page.
+
+        Cette fonction extrait les liens de la page et les ajoute à la liste des URLs à visiter.
+        """
         random.seed(229)
         soup = BeautifulSoup(html_contents, 'html.parser')
         links = soup.find_all('a', href=True)
         links = random.sample(links, 5, ) if len(links) > 5 else links
         for link in links : 
             absolute_url = urljoin(self.start_url, link['href'])
-            #print(f"Absolute link : {absolute_url}")
             if absolute_url not in self.visited_urls and absolute_url not in self.to_visit_urls :
                 self.to_visit_urls.append(absolute_url)
 
-###################LES DEUX FONCTIONS CI-DESSOUS SONT DEFINIS POUT LA BASE DE DONNEES##################
     def create_table(self):
+        """
+        Crée la table crawled_webpages dans la base de données SQLite si elle n'existe pas.
+        """
         with self.conn:
             self.conn.execute('''
                                 CREATE TABLE IF NOT EXISTS crawled_webpages 
@@ -102,20 +151,31 @@ class Crawler :
                                 age INTEGER) ''')
             
     def insert_webpage(self, url, age):
+        """
+        Insère une page crawlée dans la base de données SQLite.
+
+        Paramètres :
+        - url : URL de la page crawlée.
+        - age : Âge de la page en secondes.
+        """
         with self.conn:
             cursor = self.conn.cursor()
             cursor.execute('INSERT INTO crawled_webpages (url,  age) VALUES (?, ?)', (url,  age))
-########################################################################################################
-            
+    
     def write_to_file(self) :
+        """
+        Écrit les URLs visitées dans un fichier texte.
+        """
         with open("./crawler/crawled_webpages.txt", "w") as f :
             for url in self.visited_urls : 
                 f.write(url + "\n")
                 print(f"Writing {url} in crawled_webpages.txt")
 
-    ############# LA FONCTION CI-DESSOUS A CHANGE POUR PRENDRE EN COMPTE LE SITEMAP################
     def crawl(self):
-        while self.to_visit_urls and len(self.visited_urls) <self.max_urls : 
+        """
+        Fonction principale pour le crawling séquentiel.
+        """
+        while self.to_visit_urls and len(self.visited_urls) < self.max_urls : 
             current_url = self.to_visit_urls.pop(0)
             if current_url.endswith("sitemap.xml"):
                 self.to_visit_urls.extend(self.fetch_sitemap(current_url))
@@ -134,25 +194,34 @@ class Crawler :
                             print(f"Visited URLs : {len(self.visited_urls)}")
                             print(f"To visit URLs : {len(self.to_visit_urls)}")
 
-                        # Adjust politeness delay based on the last page download time
-                        adjusted_delay = max(self.politeness_delay, self.last_page_download_time * 2)
-                        time.sleep(adjusted_delay)
+                            # Adjust politeness delay based on the last page download time
+                            adjusted_delay = max(self.politeness_delay, self.last_page_download_time * 2)
+                            time.sleep(adjusted_delay)
 
-                        age = int(time.time()) - self.timestamp
-                        self.insert_webpage(current_url, age)
-                        self.timestamp = int(time.time())
+                            age = int(time.time()) - self.timestamp
+                            self.insert_webpage(current_url, age)
+                            self.timestamp = int(time.time())
         self.write_to_file()
-    
-    ###########################################MULTI-THREAD CRAWL######################################
+
     def insert_webpage_multi_thread(self, url, age):
+        """
+        Insère une page crawlée dans la base de données SQLite (version multi-thread).
+
+        Paramètres :
+        - url : URL de la page crawlée.
+        - age : Âge de la page en secondes.
+        """
         thread_id = threading.get_ident()
         connection = sqlite3.connect("crawler/crawler_db.sqlite")
 
         with connection:
             cursor = connection.cursor()
             cursor.execute('INSERT INTO crawled_webpages (url, age) VALUES (?, ?)', (url, age))
-    
+
     def process_page(self) : 
+        """
+        Fonction pour le traitement de chaque page dans le mode multi-threaded.
+        """
         while True :
             current_url = self.thread_queue.get()
             if current_url is None :
@@ -177,15 +246,18 @@ class Crawler :
                                     print(f"Visited URLs : {len(self.visited_urls)}")
                                     print(f"To visit URLs : {len(self.to_visit_urls)}")
 
-                                # Adjust politeness delay based on the last page download time
-                                adjusted_delay = max(self.politeness_delay, self.last_page_download_time * 2)
-                                time.sleep(adjusted_delay)
+                                    # Adjust politeness delay based on the last page download time
+                                    adjusted_delay = max(self.politeness_delay, self.last_page_download_time * 2)
+                                    time.sleep(adjusted_delay)
 
-                                age = int(time.time()) - self.timestamp
-                                self.insert_webpage_multi_thread(current_url, age)
+                                    age = int(time.time()) - self.timestamp
+                                    self.insert_webpage_multi_thread(current_url, age)
             self.thread_queue.task_done()
     
     def multi_thread_crawl(self) :
+        """
+        Fonction principale pour le crawling en mode multi-threaded.
+        """
         for i in range(self.num_threads) :
             thread = threading.Thread(target=self.process_page)
             thread.daemon = True
