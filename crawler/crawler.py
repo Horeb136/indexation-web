4a616b4daa78f07e0a13d572b6c8b2fb3bafae5b
Horeb136 <horebseidou136@gmail.com>
2024-01-30 01:50:50 +0100

    Finalisation de toutes les questions, mais reste à vérifier le dernier point

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -7,11 +7,12 @@ import xml.etree.ElementTree as ET
 import random
 import ssl
 import sqlite3
+import concurrent.futures
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 class Crawler : 
-    def __init__(self, start_url, max_urls = 50, politeness_delay = 5, db_name = "crawler_db.sqlite") : 
+    def __init__(self, start_url, max_urls = 50, politeness_delay = 5, db_name = "crawler/crawler_db.sqlite") : 
         self.start_url = start_url 
         self.max_urls = max_urls 
         self.politeness_delay = politeness_delay 
@@ -22,6 +23,7 @@ class Crawler :
         #Initialiser la connexion à la base de données
         self.conn = sqlite3.connect(db_name)
         self.create_table()
+        self.timestamp = 0
 
     def can_crawl(self, base_url) :
         if base_url.startswith("mailto:") :
@@ -59,21 +61,34 @@ class Crawler :
         except Exception as e :
             print(f"Error fetching sitemap : {e}")
             return []
-        
-        
-
 
     def extract_links(self, html_contents):
         random.seed(229)
         soup = BeautifulSoup(html_contents, 'html.parser')
         links = soup.find_all('a', href=True)
-        links = random.sample(links, 5, ) if len(links) > 5 else links
+        links = random.sample(links, 10, ) if len(links) > 10 else links
         for link in links : 
             absolute_url = urljoin(self.start_url, link['href'])
             #print(f"Absolute link : {absolute_url}")
             if absolute_url not in self.visited_urls and absolute_url not in self.to_visit_urls :
                 self.to_visit_urls.append(absolute_url)
    
+    def create_table(self):
+        with self.conn:
+            self.conn.execute('''
+                                CREATE TABLE IF NOT EXISTS crawled_webpages 
+                                (id INTEGER PRIMARY KEY, 
+                                url TEXT, 
+                                timestamp INTEGER, 
+                                age INTEGER) ''')
+            
+    def insert_webpage(self, url, age):
+        timestamp = int(time.time())
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute('INSERT INTO crawled_webpages (url,  age) VALUES (?, ?)', (url,  age))
+            self.timestamp = timestamp
+    
     def write_to_file(self) :
         with open("./crawler/crawled_webpages.txt", "w") as f :
             for url in self.visited_urls : 
@@ -109,6 +124,47 @@ class Crawler :
                         # Adjust politeness delay based on the last page download time
                         adjusted_delay = max(self.politeness_delay, self.last_page_download_time * 2)
                         time.sleep(adjusted_delay)
+
+                        age = int(time.time()) - self.timestamp
+                        self.insert_webpage(current_url, age)
             #print(f"Sites to visites : \n {self.to_visit_urls}")
         self.write_to_file()
     
+    ###########################################MULTI-THREAD CRAWL######################################
+    def process_page(self, current_url) : 
+        if not self.can_crawl(current_url) :
+                print(f"Skipping crawling {current_url} as it is not allowed by robots.txt")
+                return
+        
+        else : 
+            if current_url not in self.visited_urls : 
+                print(f"Crawling {current_url} ... ")
+                html_contents = self.fetch_page(current_url)
+
+                if html_contents : 
+                    self.extract_links(html_contents)
+                    self.visited_urls.add(current_url)
+                    print(f"Visited URLs : {len(self.visited_urls)}")
+                    print(f"To visit URLs : {len(self.to_visit_urls)}")
+
+                    # Fetch and add URLs from sitemap
+                    sitemap_urls = self.fetch_sitemap(current_url)
+                    for sitemap_url in sitemap_urls :
+                        #print(f"Fetching sitemap for {sitemap_url} ... ")
+                        if sitemap_url not in self.visited_urls and sitemap_url not in self.to_visit_urls :
+                            self.to_visit_urls.append(sitemap_url)
+                    print(f"To visit URLs after sitemap : {len(self.to_visit_urls)}")
+
+                    # Adjust politeness delay based on the last page download time
+                    adjusted_delay = max(self.politeness_delay, self.last_page_download_time * 2)
+                    time.sleep(adjusted_delay)
+
+                    age = int(time.time()) - self.timestamp
+                    self.insert_webpage(current_url, age)
+    
+    def multi_thread_crawl(self) :
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            while self.to_visit_urls and len(self.visited_urls) < self.max_urls :
+                current_url = self.to_visit_urls.pop(0)
+                executor.submit(self.process_page, current_url)
+        self.write_to_file()
--- crawler/main.py
+++ crawler/main.py
@@ -6,10 +6,14 @@ def main() :
     parser.add_argument("start_url", help="The starting URL to crawl")
     parser.add_argument("--max_urls", type=int, default=50, help="The maximum number of URLs to crawl")
     parser.add_argument("--politeness_delay", type=int, default=5, help="The politeness delay in seconds")
+    parser.add_argument("--multi_thread", type=bool, default=False, help="Enable multi-threading")
     args = parser.parse_args()
 
     crawler = Crawler(args.start_url, args.max_urls, args.politeness_delay)
-    crawler.crawl()
-
+    if args.multi_thread :
+        print("Multi-threading enabled")
+        crawler.multi_thread_crawl()
+    else :
+        crawler.crawl()
 if __name__ == "__main__" :
     main()
